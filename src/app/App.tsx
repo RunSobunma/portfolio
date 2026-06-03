b6092a1b5a5ca7b2047f70fcad6768ed42e41601
Run-Sobunma168 <panhaom@gmail.com>
2026-06-03 22:49:19 +0700

    Fix responsive

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -46,7 +46,7 @@ export default function App() {
   }
 
   return (
-    <div className="relative bg-background text-foreground overflow-x-hidden h-dvh">
+    <div className="relative bg-background text-foreground overflow-x-hidden min-h-dvh">
       <ParticlesBackground />
       <Navbar />
 
